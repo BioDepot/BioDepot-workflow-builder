53bb83750f56f85f548ea986d7bec18fa1d54cd7
Jiaming <hujiaming@hotmail.com>
2017-04-20 23:37:39 -0700

    Remove progress bar, change the ui and add experiment design file selection [dtoxs analysis]

--- biodepot/orangebiodepot/OWDtoxsAnalysis.py
+++ biodepot/orangebiodepot/OWDtoxsAnalysis.py
@@ -3,6 +3,7 @@ import Orange.data
 from Orange.data.io import FileFormat
 from Orange.widgets import widget, gui
 from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5 import QtGui, QtWidgets, QtCore
 from orangebiodepot.util.DockerClient import DockerClient, PullImageThread
 
 class OWDtoxsAnalysis(widget.OWWidget):
@@ -15,31 +16,158 @@ class OWDtoxsAnalysis(widget.OWWidget):
     image_name = "biodepot/dtoxs_analysis"
     image_version = "latest"
 
-    inputs = [("Counts", str, "set_aligns")]
+    inputs = [("Counts", str, "set_aligns", widget.Default)]
     outputs = [("Results", str),("Top 40", Orange.data.Table)]
 
-    want_main_area = False
+    want_main_area = True
+    want_control_area = False
 
     host_counts_dir = ""
+    Exp_Design_file = ""
 
 
     def __init__(self):
         super().__init__()
 
-        self.result_folder_name = "/analysis_results"
+        self.result_folder_name = "/Results"
 
         # This client talks to your local docker
         self.docker = DockerClient('unix:///var/run/docker.sock', 'local')
 
         # GUI
-        box = gui.widgetBox(self.controlArea, "Info")
-        self.infoLabel = gui.widgetLabel(box, 'Connect to Dtoxs Alignment or use a Directory widget'
-                                              ' to specify location of UMI read counts data files')
-        self.infoLabel.setWordWrap(True)
+        self.setStyleSheet("QPushButton{\n"
+                           "    background-color: #1588c5;\n"
+                           "    color: white;\n"
+                           "    height: 25px;\n"
+                           "    border: 1px solid #1a8ac6;\n"
+                           "}")
+        # creat controls
+        self.vlayoutBase = QtWidgets.QVBoxLayout()
+        self.vlayoutBase.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.vlayoutBase.setContentsMargins(0, 0, 0, 0)
+        self.vlayoutBase.setSpacing(0)
+        self.vlayoutBase.setObjectName("vlayoutBase")
 
-        self.autoRun = True
-        gui.checkBox(self.controlArea, self, 'autoRun', 'Run automatically when input set')
-        self.btn_run = gui.button(self.controlArea, self, "Run", callback=self.start_analysis)
+        self.vlayoutBase = QtWidgets.QVBoxLayout()
+        self.vlayoutBase.setContentsMargins(0, 0, 0, 0)
+        self.vlayoutBase.setSpacing(0)
+        self.vlayoutBase.setObjectName("vlayoutBase")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frameTitle = QtWidgets.QFrame(self.mainArea)
+        self.frameTitle.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frameTitle.sizePolicy().hasHeightForWidth())
+        self.frameTitle.setSizePolicy(sizePolicy)
+        self.frameTitle.setMinimumSize(QtCore.QSize(0, 45))
+        self.frameTitle.setMaximumSize(QtCore.QSize(16777215, 65))
+        self.frameTitle.setStyleSheet("QFrame#frameTitle{\n"
+                                      "    background: #1588c5;\n"
+                                      "    color: #1588c5\n"
+                                      "}")
+        self.frameTitle.setFrameShape(QtWidgets.QFrame.Box)
+        self.frameTitle.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.frameTitle.setObjectName("frameTitle")
+        self.lblFormTitle = QtWidgets.QLabel(self.frameTitle)
+        self.lblFormTitle.setGeometry(QtCore.QRect(10, 13, 221, 21))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        font.setBold(False)
+        font.setWeight(50)
+        self.lblFormTitle.setFont(font)
+        self.lblFormTitle.setStyleSheet("\n"
+                                        "    color: white;\n"
+                                        "")
+        self.lblFormTitle.setObjectName("lblFormTitle")
+        self.verticalLayout.addWidget(self.frameTitle)
+        self.mainContent = QtWidgets.QWidget(self.mainArea)
+        self.mainContent.setObjectName("mainContent")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.mainContent)
+        self.verticalLayout_4.setContentsMargins(15, 15, 15, 15)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.vlayout_content = QtWidgets.QVBoxLayout()
+        self.vlayout_content.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.vlayout_content.setSpacing(2)
+        self.vlayout_content.setObjectName("vlayout_content")
+        self.lblExpFile = QtWidgets.QLabel(self.mainContent)
+        self.lblExpFile.setMinimumSize(QtCore.QSize(0, 25))
+        self.lblExpFile.setMaximumSize(QtCore.QSize(16777215, 25))
+        self.lblExpFile.setObjectName("lblExpFile")
+        self.vlayout_content.addWidget(self.lblExpFile)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.horizontalLayout.setContentsMargins(0, -1, -1, -1)
+        self.horizontalLayout.setSpacing(1)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.edtExpDesignFile = QtWidgets.QLineEdit(self.mainContent)
+        self.edtExpDesignFile.setObjectName("edtExpDesignFile")
+        self.horizontalLayout.addWidget(self.edtExpDesignFile)
+        self.btnSelectExpFile = QtWidgets.QPushButton(self.mainContent)
+        self.btnSelectExpFile.setMinimumSize(QtCore.QSize(24, 24))
+        self.btnSelectExpFile.setMaximumSize(QtCore.QSize(24, 24))
+        self.btnSelectExpFile.setObjectName("btnSelectExpFile")
+        self.horizontalLayout.addWidget(self.btnSelectExpFile)
+        self.vlayout_content.addLayout(self.horizontalLayout)
+        self.line_2 = QtWidgets.QFrame(self.mainContent)
+        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.vlayout_content.addWidget(self.line_2)
+        self.lblInfoTitle = QtWidgets.QLabel(self.mainContent)
+        self.lblInfoTitle.setMinimumSize(QtCore.QSize(0, 25))
+        self.lblInfoTitle.setMaximumSize(QtCore.QSize(16777215, 25))
+        self.lblInfoTitle.setStyleSheet(
+            "background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(235, 235, 235, 255), stop:1 rgba(217, 217, 217, 255));\n"
+            "padding-left: 3px;")
+        self.lblInfoTitle.setObjectName("lblInfoTitle")
+        self.vlayout_content.addWidget(self.lblInfoTitle)
+        self.line = QtWidgets.QFrame(self.mainContent)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.vlayout_content.addWidget(self.line)
+        self.infoLabel = QtWidgets.QLabel(self.mainContent)
+        self.infoLabel.setMinimumSize(QtCore.QSize(0, 45))
+        self.infoLabel.setStyleSheet("padding-left: 3px")
+        self.infoLabel.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.infoLabel.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.infoLabel.setWordWrap(True)
+        self.infoLabel.setObjectName("infoLabel")
+        self.vlayout_content.addWidget(self.infoLabel)
+        self.btn_run = QtWidgets.QPushButton(self.mainContent)
+        self.btn_run.setMinimumSize(QtCore.QSize(90, 0))
+        self.btn_run.setMaximumSize(QtCore.QSize(90, 16777215))
+        self.btn_run.setObjectName("btn_run")
+        self.vlayout_content.addWidget(self.btn_run, 0, QtCore.Qt.AlignRight)
+        self.verticalLayout_4.addLayout(self.vlayout_content)
+        self.verticalLayout.addWidget(self.mainContent)
+        self.vlayoutBase.addLayout(self.verticalLayout)
+
+        self.mainArea.layout().addLayout(self.vlayoutBase)
+        self.mainArea.layout().setContentsMargins(0, 0, 0, 0)
+        self.mainArea.setMinimumSize(568, 246)
+
+        # events
+        self.btnSelectExpFile.clicked.connect(self.OnChooseExpFile)
+        self.btn_run.clicked.connect(self.start_analysis)
+
+        self.retranslateUi(self.mainArea)
+
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Widget", "Form"))
+        self.lblFormTitle.setText(_translate("Widget", "DTOXS Analysis"))
+        self.lblExpFile.setText(_translate("Widget", "Select experiment design file:"))
+        self.btnSelectExpFile.setText(_translate("Widget", " ☰ "))
+        self.lblInfoTitle.setText(_translate("Widget", "Information"))
+        self.infoLabel.setText(_translate("Widget",
+                                          "Connect to Dtoxs Alignment or use a Directory widget to specify location of UMI read counts data files"))
+        self.btn_run.setText(_translate("Widget", "Run"))
 
     """
     Set input
@@ -59,10 +187,25 @@ class OWDtoxsAnalysis(widget.OWWidget):
             if not os.path.exists(self.host_results_dir):
                 os.makedirs(self.host_results_dir)
 
-            if self.autoRun:
-                self.start_analysis()
-            else:
-                self.infoLabel.setText('Alignment directory set.\nWaiting to run...')
+            if self.Exp_Design_file != "":
+                import shutil
+                shutil.copy2(self.Exp_Design_file, self.host_counts_dir)
+
+            self.infoLabel.setText('Alignment directory set.\nWaiting to run...')
+
+
+    def OnChooseExpFile(self):
+        start_file = os.path.expanduser("~/")
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Open Experiment Design File', start_file)
+        if not filename:
+            return
+
+        self.edtExpDesignFile.setText(filename)
+        self.Exp_Design_file = filename
+        if self.host_counts_dir != "":
+            import shutil
+            shutil.copy2(self.Exp_Design_file, self.host_counts_dir)
 
     """
     Pull image
@@ -102,7 +245,7 @@ class OWDtoxsAnalysis(widget.OWWidget):
     def run_analysis(self):
         self.infoLabel.setText('Running analysis...')
         self.setStatusMessage('Running...')
-        self.progressBarInit()
+        #self.progressBarInit()
         # Run the container in a new thread
         self.run_analysis_thread = RunAnalysisThread(self.docker,
                                                      self.image_name,
@@ -121,7 +264,7 @@ class OWDtoxsAnalysis(widget.OWWidget):
         self.btn_run.setEnabled(True)
         self.btn_run.setText('Run again')
         self.setStatusMessage('Finished!')
-        self.progressBarFinished()
+        #self.progressBarFinished()
         self.send("Results", self.host_results_dir)
 
         # Jimmy March 29 2017 added, create a dataset for DataTable widget to show TOP-40.tsv
@@ -195,11 +338,21 @@ class RunAnalysisThread(QThread):
         else:
             # TODO emit warning to Widget
             print(response['Warnings'])
-        i = 1
+        #i = 1
         # Keep running until container is exited
         while self.docker.container_running(self.containerId):
-            self.analysis_progress.emit(i * 0.55)
-            self.sleep(1)
-            i += 1
+            #self.analysis_progress.emit(i * 0.55)
+            self.sleep(2)
+            #i += 1
         # Remove the container now that it is finished
         self.docker.remove_container(self.containerId)
+
+if __name__ == "__main__":
+    import sys
+    from AnyQt.QtWidgets import QApplication
+
+    a = QApplication(sys.argv)
+    ow = OWDtoxsAnalysis()
+    ow.show()
+    a.exec_()
+    ow.saveSettings()
