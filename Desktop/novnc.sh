51988f08390a552ab959742617b04dbb2dcdbd2b
Jiaming <hujiaming@hotmail.com>
2017-05-21 20:44:17 -0700

    Change the resolution 1024x800 for VNC

--- Desktop/novnc.sh
+++ Desktop/novnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISPLAY=:1
-Xvfb :1 -screen 0 1024x600x16 & 
+Xvfb :1 -screen 0 1024x800x16 & 
 sleep 5
 openbox-session & xsetroot -solid "#FFFFFF" &
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
