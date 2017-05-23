679f4586941c845e36a49ada1b7c13b53f020ca0
Jiaming <hujiaming@hotmail.com>
2017-05-23 11:14:03 -0700

    rename vnc_auto to vnc_lite
    noVNC changed this file name

--- Desktop/novnc.sh
+++ Desktop/novnc.sh
@@ -4,4 +4,4 @@ Xvfb :1 -screen 0 1024x800x16 &
 sleep 5
 openbox-session & xsetroot -solid "#FFFFFF" &
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 
+cd /root/noVNC && ln -s vnc_lite.html index.html && ./utils/launch.sh --vnc localhost:5900 
