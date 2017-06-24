76e0f689583f7e4f491b1ab20edddb7df3c7c002
Jiaming <hujiaming@hotmail.com>
2017-06-23 21:57:33 -0700

    Add base widget class OWBwBWidget and LocalContainerRunner

---
+++ biodepot/orangebiodepot/util/BwBase.py
@@ -0,0 +1,137 @@
+import os
+from AnyQt.QtCore import QThread, pyqtSignal
+from Orange.widgets import widget
+from orangebiodepot.util.DockerClient import DockerClient, PullImageThread
+
+class LocalContainerRunner(QThread):
+    progress = pyqtSignal(int)
+
+    def __init__(self, cli, image_name, volumes, commands, environments = None):
+        QThread.__init__(self)
+        self.docker = cli
+        self.image_name = image_name
+        self.volumes = volumes
+        self.commands = commands
+        self.environments = environments
+        self.containerId = ""
+        self.Flag_isRunning = False
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        response = self.docker.create_container(self.image_name, volumes=self.volumes, commands=self.commands, environment=self.environments)
+        if response['Warnings'] == None:
+            self.containerId = response['Id']
+            self.docker.start_container(self.containerId)
+        else:
+            print(response['Warnings'])
+
+        # Keep running until container is exited
+        while self.docker.container_running(self.containerId):
+            self.sleep(1)
+        # Remove the container when it is finished
+        self.docker.remove_container(self.containerId)
+
+
+class OWBwBWidget(widget.OWWidget):
+
+    dockerClient = DockerClient('unix:///var/run/docker.sock', 'local')
+
+    def __init__(self, image_name, image_tag):
+        super().__init__()
+        self._hostDirectories = {}
+        self._dockerImageName = image_name
+        self._dockerImageTag = image_tag
+        self._dockerVolumes = None
+        self._dockerCommands = None
+        self._dockerEnvironments = None
+        self._Flag_isRunning = False
+
+    def Event_OnRunFinished(self):
+        raise Exception('Event_OnRunFinished not implemented!')
+
+    def Event_OnRunMessage(self, message):
+        raise Exception('Event_OnRunMessage not implemented!')
+
+    def Initialize_InputKeys(self, keys):
+        for k in keys:
+            self._hostDirectories[k] = None
+
+    def setDirectories(self, key, path, ctrlLabel = None):
+        labelTitle = ctrlLabel.text().split(':')[0] + ': {}'
+        # When a user removes a connected Directory widget,
+        # it sends a signal with path=None
+        if path is None:
+            self._hostDirectories[key] = None
+            if ctrlLabel is not None: ctrlLabel.setText(labelTitle.format('Not Set'))
+        elif not os.path.exists(path):
+            self._hostDirectories[key] = None
+            if ctrlLabel is not None: ctrlLabel.setText(labelTitle.format('None existent directory: {}'.format(str(path))))
+        else:
+            self._hostDirectories[key] = path.strip()
+            if ctrlLabel is not None: ctrlLabel.setText(labelTitle.format((str(path))))
+
+
+    def getDirectory(self, key):
+        return self._hostDirectories[key]
+
+    """
+    Pull image
+    """
+    def __dockerPullImage__(self):
+        self.Event_OnRunMessage('Pulling \'' + self._dockerImageName + ":" + self._dockerImageTag+ '\' from Dockerhub...')
+        self.setStatusMessage("Downloading...")
+        self.progressBarInit()
+        self._Flag_isRunning = True
+        # Pull the image in a new thread
+        self.pullImageThread = PullImageThread(self.dockerClient, self._dockerImageName, self._dockerImageTag)
+        self.pullImageThread.pull_progress.connect(self.__dockerPullProgress__)
+        self.pullImageThread.finished.connect(self.__dockerPullDone__)
+        self.pullImageThread.start()
+
+    def __dockerPullProgress__(self, val):
+        self.progressBarSet(val)
+
+    def __dockerPullDone__(self):
+        self.Event_OnRunMessage('Finished pulling \'' + self._dockerImageName + ":" + self._dockerImageTag + '\'')
+        self.progressBarFinished()
+        self.__dockerRealRun__()
+
+    """
+    Run container
+    """
+    def dockerRun(self, volumes = None, commands = None, environments = None):
+        if not self._Flag_isRunning:
+            self._dockerVolumes = volumes
+            self._dockerCommands = commands
+            self._dockerEnvironments = environments
+            # Make sure the docker image is downloaded
+            if not self.dockerClient.has_image(self._dockerImageName, self._dockerImageTag):
+                self.__dockerPullImage__()
+            else:
+                self.__dockerRealRun__()
+
+    def __dockerRealRun__(self):
+        self._Flag_isRunning = True
+        self.setStatusMessage('Running...')
+        self.Event_OnRunMessage('Running \'' + self._dockerImageName + '\'')
+        # Run the container in a new thread
+        self.containerThread = LocalContainerRunner(
+                                self.dockerClient,
+                                '{}:{}'.format(self._dockerImageName, self._dockerImageTag),
+                                self._dockerVolumes,
+                                self._dockerCommands,
+                                self._dockerEnvironments)
+        self.containerThread.progress.connect(self.__dockerRunProgress__)
+        self.containerThread.finished.connect(self.__dockerRunDone__)
+        self.containerThread.start()
+
+    def __dockerRunProgress__(self, val):
+        self.progressBarSet(val)
+
+    def __dockerRunDone__(self):
+        self._Flag_isRunning = False
+        self.setStatusMessage('Finished!')
+        self.Event_OnRunMessage('Finished!')
+        self.Event_OnRunFinished()
