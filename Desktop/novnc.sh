e031f3bcae6609a482c7494a464b51fb51b346e7
Jiaming <hujiaming@hotmail.com>
2017-06-23 21:59:58 -0700

    remove '-ncache 10 -ncache_cr' which cause weird novnc behavior

--- Desktop/novnc.sh
+++ Desktop/novnc.sh
@@ -3,5 +3,5 @@ export DISPLAY=:1
 Xvfb :1 -screen 0 1024x800x16 & 
 sleep 5
 openbox-session & xsetroot -solid "#FFFFFF" &
-x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+x11vnc -display :1 -nopw -listen localhost -xkb -forever &
 cd /root/noVNC && ln -s vnc_lite.html index.html && ./utils/launch.sh --vnc localhost:5900 
