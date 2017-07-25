576f594c19ab6b327e5696b32f4b329f32edd60b
Jiaming <hujiaming@hotmail.com>
2017-07-24 22:27:09 -0700

    update bam2counts image
    The true name of this image should be biodepot/bam2deseq

--- Widgets/bam2counts/bam2counts.R
+++ Widgets/bam2counts/bam2counts.R
@@ -1,11 +1,10 @@
 
 cmd_args <- commandArgs(TRUE)
-if(length(cmd_args) < 3){
-    cat(paste("Usage: Rscript bam2counts.R [GTF File] [Sample Table File] [BAM file Directory] [Workers number]\n"))
+if(length(cmd_args) < 2){
+    cat(paste("Usage: Rscript bam2counts.R [GTF File] [BAM file Directory] [Workers number]\n"))
     cat("\n")
     cat("Arguments:\n\n")
     cat("\t[GTF File] a file specifying the genomic features.\n\n")
-    cat("\t[Sample Table File] is a table with detailed information for each of samples that links samples to the associated FASTQ and BAM files.\n\n")
     cat("\t[BAM file Directory] is a directory contains BAM files.\n\n")
     cat("\t[Workers number] is a parameter defined number of threads, default = 1")
     stop()
@@ -14,19 +13,15 @@ if(length(cmd_args) < 3){
 start.time <- Sys.time()
 
 gtf_file <- cmd_args[1]
-sample_table <- cmd_args[2]
-bam_filepath <- cmd_args[3]
+bam_filepath <- cmd_args[2]
 workers_number <- 1
-if(length(cmd_args)>=4){
-    workers_number <- cmd_args[4]
+if(length(cmd_args)>=3){
+    workers_number <- cmd_args[3]
 }
 
 print(paste0("Working threads: ", workers_number))
 
-print("Loading sample table....")
-sampleTable <- read.csv(sample_table, row.names = 1)
-
-print("Loading GTF file....")
+print(paste0("Loading GTF file....", gtf_file))
 library("GenomicFeatures")
 txdb <- makeTxDbFromGFF(gtf_file, format = "gtf", circ_seqs = character())
 ebg <- exonsBy(txdb, by="gene")
@@ -61,24 +56,11 @@ se_file <- file.path(bam_filepath, "se.RDATA")
 print(paste0("Saving se data to: ", se_file))
 save(se, file=se_file)
 
-library("DESeq2")
-print("DESeq......")
-colData(se) <- DataFrame(sampleTable)
+bam_counts = assay(se)
+counts_file <- file.path(bam_filepath, "bamcounts.csv")
+print(paste0("Saving counts table to: ", counts_file))
+write.table(bam_counts, file=counts_file, sep = ",")
 
-dds <- DESeqDataSet(se, design = ~ cell + dex)
-
-dds_file <- file.path(bam_filepath, "dds.RDATA")
-
-print(paste0("Saving dds to: ", dds_file))
-
-save(dds, file=dds_file)
-
-result_file <- file.path(bam_filepath, "deseq_results.csv")
-print(paste0("Generating and saving result table to: ", result_file))
-dds <- DESeq(dds)
-res <- results(dds)
-resOrdered <- res[order(res$pvalue),]
-write.csv(as.data.frame(resOrdered), file=result_file)
 
 end.time <- Sys.time()
 time.taken <- end.time - start.time
