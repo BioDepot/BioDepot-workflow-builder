07a8d16adf3dc72a36242802e81cf404d2ce5652
Git-Admin <lhhunghimself@gmail.com>
2018-03-15 15:11:29 -0700

    added editing to qwidgetlists in widget builder

--- biodepot/orangebiodepot/OWWidgetBuilder.py
+++ biodepot/orangebiodepot/OWWidgetBuilder.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+from collections import OrderedDict
 from functools import partial
 from AnyQt.QtCore import QThread, pyqtSignal, Qt
 from Orange.widgets import widget, gui, settings
@@ -14,27 +15,45 @@ from AnyQt.QtWidgets import (
     QSizePolicy, QApplication, QCheckBox
 )
 
-class NamedDict():
-    def __init__(self):
-        name=None
-        _dict={}
+class WidgetItem():
+    #each widget item has:
+    #A set of gui widgets that correspond to the name and each parameter
+    #A set of state vectors corresponding to the different elements that make up the item
         
-    def setName(self,inputName):
-        self.name=inPutname
+    def __init__(self, guiElements=OrderedDict(), states={}):
+        #guielements has parameter as key and qwidget as value
+        self.guiElements=guiElements
+        self.states=states
+        if self.states and self.guiElements:
+            for (key,element) in self.guiElements.items():
+                element.setState(self.states[key])
+        elif self.guiElements:
+            self.blankState()
+                
+    def printValue(self):
+        output='{} : {{'.format(self.guiElements['name'].getValue())
+        for key, element in self.guiElements.items():
+            if key != 'name' and element.getValue() is not None:
+                output+=' {} : {}, '.format(key,element.getValue())
+        output=output[:-2]+' }'
+        return output
         
-    def setElement(self,key,value):
-        self._dict[key]=value
+    def getState(self):
+        states={}
+        for key, element in self.guiElements.items():
+           states[key]=element.getState()
+        return states
         
-    def delElement(self,key):
-        del self._dict[key]
-    
-    def printItem(self):
-        outputStr="{}, {{ ".format(name)
-        for key, value in sorted(self._dict.items()):
-            outputStr+="{} : {} ".format(key,value)
-        outputStr+= '}}'
-        return outputStr
-
+    def loadState(self,states={}):
+        self.states=states
+        for key, element in self.guiElements.items():
+            element.setState(self.states[key])
+            
+    def blankState(self):
+        for key, element in self.guiElements.items():
+            element.setState(None)
+            self.states[key]=None
+        
 class OWWidgetBuilder(widget.OWWidget):
     name = "WidgetBuilder"
     description = "Set one or more data files"
@@ -90,9 +109,16 @@ class OWWidgetBuilder(widget.OWWidget):
         self.drawLedit('Priority',requiredBox,layout=leditRequiredLayout)
         #file entry for icon
         self.drawFileLedit('Icon',requiredBox,layout=leditRequiredLayout)
+        #checkBox for automap Volumens
+
+        #auto
+        
         #listwidgets for inputs and outputs 
-        self.drawListWidget('Inputs',requiredBox,layout=leditRequiredLayout)
-        self.drawListWidget('Outputs',requiredBox,layout=leditRequiredLayout)
+        #define widgetItems for the different widgetLists
+        
+        self.drawIOListWidget('Inputs',requiredBox,layout=leditRequiredLayout)
+        self.drawIOListWidget('Outputs',requiredBox,layout=leditRequiredLayout)
+        self.drawCheckBox('AutoMap',requiredBox,layout=leditRequiredLayout,default=True, label='Pass current Bwb volumes to container')
         self.drawVolumeListWidget('Volumes',requiredBox,layout=leditRequiredLayout)
         self.drawParamsListWidget('Parameters',requiredBox,layout=leditRequiredLayout)
         self.drawCommand('Commands',requiredBox,layout=leditRequiredLayout)
@@ -103,10 +129,38 @@ class OWWidgetBuilder(widget.OWWidget):
         if(checkBox.isEnabled()):
             widget.setEnabled(checkBox.isChecked())
         
-    def addListWidget(self,pname,boxEdit,comboBox,addBtn):
-        pass
-    def removeListWidget(self,pname,boxEdit,comboBox,addBtn):
-        pass 
+    def addListWidget(self,addBtn,qWidgetList,qWidgetItem):
+        if qWidgetList.selectedItems():
+            index=qWidgetList.row(qWidgetList.selectedItems()[0])
+            qWidgetList.states[index]=qWidgetItem.getState()
+            qWidgetList.selectedItems()[0].setText(qWidgetItem.printValue())
+        else:    
+            item=qWidgetItem.printValue()
+            qWidgetList.addItem(item)
+            qWidgetList.states.append(qWidgetItem.getState())
+            qWidgetItem.blankState()
+        
+    def removeListWidget(self,removeBtn,qWidgetList,qWidgetItem):
+        if qWidgetList.selectedItems():
+            for item in qWidgetList.selectedItems():
+                del qWidgetList.states[qWidgetList.row(item)]
+                qWidgetList.takeItem(qWidgetList.row(item))
+        if not qWidgetList.count():
+            removeBtn.setEnabled(False)
+        qWidgetItem.blankState()
+    
+    def onListWidgetSelect(self,qWidgetList,addBtn,removeBtn,qWidgetItem):
+        if qWidgetList.selectedItems():
+            if len(qWidgetList.selectedItems()) > 1:
+                addBtn.setEnabled(False)
+            else:
+                item=qWidgetList.selectedItems()[0]
+                myState=qWidgetList.states[qWidgetList.row(item)]
+                qWidgetItem.loadState(myState)
+                addBtn.setEnabled(True)
+        else:
+            removeBtn.setEnabled(False)
+            addBtn.setEnabled(True)
     
     def drawCommand(self,pname,box=None,layout=None):
         label=QtGui.QLabel(pname+":")
@@ -145,8 +199,67 @@ class OWWidgetBuilder(widget.OWWidget):
         if leditLabel:
             box.addWidget(leditLabel)
             box.addWidget(ledit)
+        setattr(box,'getValue',lambda : self.getLeditValue(ledit))
+        setattr(box,'getState',lambda : self.getLeditState(checkBox,leditLabel,ledit))
+        setattr(box,'setState',lambda state : self.setLeditState(checkBox,leditLabel,ledit,state))
         return box
         
+    def getLeditValue(self,ledit):
+        if ledit.isEnabled():
+            return ledit.text()
+        return None
+        
+    def getLeditState(self,checkBox,label,ledit):
+        state={}
+        if checkBox:
+            state[checkBox]=[checkBox.isEnabled() ,checkBox.isChecked()]
+        else:
+            state[checkBox]=None
+        if label:
+            state[label]=[label.isEnabled(), label.text() ]
+        else:
+            state[label]=None
+        if ledit:
+            state[ledit]=[ledit.isEnabled(), ledit.text() ]
+        else:
+            state[label]=None
+        return state
+        
+    def setLeditState(self,checkBox,label,ledit,state):
+        if state is None:
+            #initialize it
+            if checkBox:
+                checkBox.setEnabled(True)
+                checkBox.setChecked(False)
+                ledit.clear()
+                ledit.setEnabled(False)
+                label.setEnabled(True)
+            else:
+                ledit.setEnabled(True)
+                ledit.clear()
+                label.setEnabled(True)
+            return
+            
+        if state[checkBox] is None:
+            checkBox=None
+        else:
+            checkBox.setEnabled(state[checkBox][0])
+            checkBox.setChecked(state[checkBox][1])
+        if state[ledit] is None:
+            ledit=None
+        else:
+            ledit.setEnabled(state[ledit][0])
+            if state[ledit][1]:
+                ledit.setText(state[ledit][1])
+            else:
+                ledit.clear()
+        if state[label] is None:
+            label=None
+        else:
+            label.setEnabled(state[label][0])
+            label.setText(state[label][1])
+                
+       
     def makeComboBox (self,pname,label, elements):
         comboBoxLabel=QtGui.QLabel(label)
         comboBox=QtGui.QComboBox()
@@ -155,9 +268,81 @@ class OWWidgetBuilder(widget.OWWidget):
         box=QHBoxLayout()
         box.addWidget(comboBoxLabel)
         box.addWidget(comboBox)
+        setattr(box,'getValue',lambda : self.getComboValue(comboBox))
+        setattr(box,'getState',lambda : self.getComboState(comboBox,comboBoxLabel))
+        setattr(box,'setState',lambda state : self.setComboState(comboBox,comboBoxLabel,state))
         return box
         
+    def getComboValue(self,comboBox):
+        if comboBox.isEnabled():
+            return str(comboBox.currentText())
+        return None 
         
+    def getComboState(self,comboBox,label):
+        state={}
+        if comboBox:
+            allItems=[comboBox.itemText(i) for i in range (comboBox.count())]
+            enabled=comboBox.isEnabled()
+            index=comboBox.currentIndex
+            state[comboBox]=[enabled,index,allItems]
+        else:
+            state[comboBox]=None
+        if label:
+            state[label]=[label.isEnabled(),label.text()]
+        else:
+            state[label]=None
+        return state 
+    
+    def setComboState(self,comboBox,label,state):
+        if state is None:
+            #intialize
+            comboBox.setEnabled(True)
+            comboBox.setCurrentIndex(0)
+        return
+        if state[comboBox] is None:
+            comboBox=None
+        else:
+           comboBox.setEnabled(state[comboBox][0])
+           comboBox.setItems(state[comboBox][2])
+           comboBox.setCurrentIndex(state[comboBox][1])
+           
+        if state[label] is None:
+            label=None
+        else:
+            label.setEnabled(state[label][0])
+            label.setText(state[label][1])
+        
+    def makeCheckBox (self, pname,label):
+        #checkbox for binary options
+        #not used as part of other elements
+        setattr(self,'pname',False)
+        checkBox=gui.checkBox(None, self, pname, label)
+        setattr(checkBox,'getValue',lambda : checkBox.isEnabled() and checkBox.isChecked())
+        setattr(checkBox,'getState',lambda : self.getCheckBoxState(checkBox,label))
+        setattr(checkBox,'setState',lambda state : self.setCheckBoxState(checkBox,label,state))
+        return checkBox
+     
+    def getCheckBoxState(self,checkBox,label):
+        state={}
+        if checkBox:
+            state[checkBox]=[checkBox.isEnabled() ,checkBox.isChecked()]
+        else:
+            state[checkBox]=None
+        return state
+        
+    def setCheckBoxState(self,checkBox,label,state):
+        if state is None:
+            #initialize
+            checkBox.setEnabled(True)
+            checkBox.setChecked(False)
+            return
+            
+        if state[checkBox] is None:
+            checkBox=None
+        else:
+            checkBox.setEnabled(state[checkBox][0])
+            checkBox.setChecked(state[checkBox][1])   
+    
     def makeListWidget(self,pname,boxEdit):
         #setup boxEdit
         #logic is handled by add remove buttons
@@ -171,27 +356,43 @@ class OWWidgetBuilder(widget.OWWidget):
         return boxEdit
         
     #multiple widget elements       
-    def drawListWidget (self, pname, box=None, layout=None):
+    def drawIOListWidget (self, pname, box=None, layout=None, attrValues=None):
         
-        #save items to qlist for ordered Dict
-        #each item in the list is an attribute and a dict
-            
+        #the structure for values element of the widget list is:
+        # (name,{}) i.e. a pair of str and dict
+        # we have a definition list to tell us which values are associated with which elements
+        # onchange of the element, the value gets changed
+        # This is an orderdict of elements and keyvalues
+        # all of this is stored in a class for widgetItem
+    
+        
         #setup boxEdit
         boxEdit=self.makeListWidget(pname,None)
+        setattr(boxEdit,'states',[])
+
 
         #ledit
         checkBox=None
         nameBox=self.makeLedit(pname+'nameLedit','Enter name','Name')
         callbackBox=self.makeLedit(pname+'callbackLedit','Enter callback', 'Callback',addCheckBox=True)
-
-        #buttons
-        addBtn=gui.button(None, self, "", callback=lambda: self.addListWidget(pname,boxEdit,comboBox,addBtn), autoDefault=False)
-        removeBtn=gui.button(None, self, "", callback=lambda: self.removeListWidget(pname,boxEdit,removeBtn), autoDefault=False)
-        addBtn.setIcon(self.addIcon)
-        removeBtn.setIcon(self.removeIcon)
         
         #comboBox
         comboBox=self.makeComboBox(pname,'Type:',['str'])
+        
+        lineItem=WidgetItem(OrderedDict([('name',nameBox),('type',comboBox),('Callback',callbackBox)]))
+        
+        #buttons
+        addBtn=gui.button(None, self, "", callback=lambda: self.addListWidget(addBtn,boxEdit,lineItem), autoDefault=False)
+        removeBtn=gui.button(None, self, "", callback=lambda: self.removeListWidget(removeBtn,boxEdit,lineItem), autoDefault=False)
+        removeBtn.setEnabled(bool(boxEdit.selectedItems()))
+        boxEdit.itemSelectionChanged.connect(lambda: removeBtn.setEnabled(bool(boxEdit.selectedItems())))
+        boxEdit.itemSelectionChanged.connect(lambda: self.onListWidgetSelect(boxEdit,addBtn,removeBtn,lineItem))
+        
+        addBtn.setIcon(self.addIcon)
+        removeBtn.setIcon(self.removeIcon)
+        
+        #set up base item
+
         
         #set styles
         buttonStyle='background: None; border: None ; border-radius: 0;'
@@ -222,30 +423,32 @@ class OWWidgetBuilder(widget.OWWidget):
         #now add the two layouts to the bigBox layout
         filesBoxLeditLayout.addWidget(boxEdit)
         filesBoxLeditLayout.addLayout(lineLayout)
+   
 
     def drawVolumeListWidget (self, pname, box=None, layout=None):
-        
-        #save items to qlist for ordered Dict
-        #each item in the list is an attribute and a dict
             
         #setup boxEdit
         boxEdit=self.makeListWidget(pname,None)
+        setattr(boxEdit,'states',[])
         
         #ledit and buttons
-        volumeBox=self.makeLedit(pname+'volumenLedit','Enter additional volume','Additional volume')
-        
+        nameBox=self.makeLedit(pname+'Name','Enter name','Name')
+        volumeBox=self.makeLedit(pname+'volumeLedit','Enter volume','Additional volume')
      
+        #define the line item being edited
+        lineItem=WidgetItem(OrderedDict([('name',nameBox),('volume',volumeBox)]),{})
+
+        
         #buttons
-        addBtn=gui.button(None, self, "", callback=lambda: self.addListWidget(pname,boxEdit,comboBox,addBtn), autoDefault=False)
-        removeBtn=gui.button(None, self, "", callback=lambda: self.removeListWidget(pname,boxEdit,removeBtn), autoDefault=False)
+        addBtn=gui.button(None, self, "", callback=lambda: self.addListWidget(addBtn,boxEdit,lineItem), autoDefault=False)
+        removeBtn=gui.button(None, self, "", callback=lambda: self.removeListWidget(removeBtn,boxEdit,lineItem), autoDefault=False)
+        removeBtn.setEnabled(bool(boxEdit.selectedItems()))
+        boxEdit.itemSelectionChanged.connect(lambda: removeBtn.setEnabled(bool(boxEdit.selectedItems())))
+        boxEdit.itemSelectionChanged.connect(lambda: self.onListWidgetSelect(boxEdit,addBtn,removeBtn,lineItem))
+
         addBtn.setIcon(self.addIcon)
         removeBtn.setIcon(self.removeIcon)
         
-        #checkBox
-        if not hasattr(self,'AutoMap'):
-            setattr(self,'AutoMap',True)
-        checkBox=gui.checkBox(box, self,'AutoMap', 'AutoMap')
-
         #set styles
         buttonStyle='background: None; border: None ; border-radius: 0;'
         addBtn.setStyleSheet(buttonStyle)
@@ -266,7 +469,7 @@ class OWWidgetBuilder(widget.OWWidget):
         
         #line layout     
         lineLayout=QtGui.QGridLayout()
-        lineLayout.addWidget(checkBox,1,0)
+        lineLayout.addLayout(nameBox,1,0)
         lineLayout.addLayout(volumeBox,1,1)
         lineLayout.addWidget(addBtn,1,2)
         lineLayout.addWidget(removeBtn,1,3)
@@ -281,7 +484,8 @@ class OWWidgetBuilder(widget.OWWidget):
             
         #setup boxEdit
         boxEdit=self.makeListWidget(pname,None)
-
+        setattr(boxEdit,'states',[])
+        
         #ledit
         checkBox=None
         nameBox=self.makeLedit(pname+'nameLedit','Enter name','Name')
@@ -289,26 +493,32 @@ class OWWidgetBuilder(widget.OWWidget):
         labelBox=self.makeLedit(pname+'labelLedit','Enter label', 'label',addCheckBox=True)
         envBox=self.makeLedit(pname+'envLedit','Enter ENV variable', 'env',addCheckBox=True)
         defaultBox=self.makeLedit(pname+'defaultLedit','Enter default', 'default',addCheckBox=True)
-        
-        #buttons
-        addBtn=gui.button(None, self, "", callback=lambda: self.addListWidget(pname,boxEdit,comboBox,addBtn), autoDefault=False)
-        removeBtn=gui.button(None, self, "", callback=lambda: self.removeListWidget(pname,boxEdit,removeBtn), autoDefault=False)
-        addBtn.setIcon(self.addIcon)
-        removeBtn.setIcon(self.removeIcon)
-        
+
         #checkBox
 
         setattr(self,'optional',False)
-        optionalCb=gui.checkBox(box, self,'optional', 'Optional')
-        
         setattr(self,'argument',False)
-        argumentCb=gui.checkBox(box, self,'argument', 'Argument')
+        optionalCb=self.makeCheckBox ('optional','Optional')
+        argumentCb=self.makeCheckBox ('argument','Argument')
         #connect argument checkbox to disabling the flag checkbox       
         argumentCb.stateChanged.connect(lambda : self.updateCheckBoxLayout(argumentCb,flagBox))
         
-        
         #comboBox
-        comboBox=self.makeComboBox(pname,'Type:',['str','file','directory','directoryList','fileList','int','double'])
+        comboBox=self.makeComboBox(pname,'Type:',['str','file','directory','directoryList','fileList','int','double'])        
+        #define the line item being edited
+        lineItem=WidgetItem(OrderedDict([('name',nameBox),('flag',flagBox), ('label',labelBox), ('env',envBox),
+                                         ('default',defaultBox),('optional',optionalCb),('argument',argumentCb),
+                                         ('type',comboBox)]),{})
+        
+        #buttons
+        addBtn=gui.button(None, self, "", callback=lambda: self.addListWidget(addBtn,boxEdit,lineItem), autoDefault=False)
+        removeBtn=gui.button(None, self, "", callback=lambda: self.removeListWidget(removeBtn,boxEdit,lineItem), autoDefault=False)
+        removeBtn.setEnabled(bool(boxEdit.selectedItems()))
+        boxEdit.itemSelectionChanged.connect(lambda: removeBtn.setEnabled(bool(boxEdit.selectedItems())))
+        boxEdit.itemSelectionChanged.connect(lambda: self.onListWidgetSelect(boxEdit,addBtn,removeBtn,lineItem))
+
+        addBtn.setIcon(self.addIcon)
+        removeBtn.setIcon(self.removeIcon)
         
         #set styles
         buttonStyle='background: None; border: None ; border-radius: 0;'
@@ -373,6 +583,13 @@ class OWWidgetBuilder(widget.OWWidget):
         ledit.setClearButtonEnabled(True)
         layout.addWidget(ledit,layout.nextRow,1,1,2)
         layout.nextRow+=1
+        
+    def drawCheckBox(self,pname,box=None,layout=None, label=None, default=False):
+        if not hasattr(self,pname):
+            setattr(self,pname,default)
+        cb=gui.checkBox(None, self,pname, label)
+        layout.addWidget(cb,layout.nextRow,0,1,2)
+        layout.nextRow+=1       
      
     def browseFileDir(self, attr):
         defaultDir = '/root'
