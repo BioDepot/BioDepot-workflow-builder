4dd9d2bc58973ae56ba6eee6308a7630d2ab8088
Git-Admin <lhhunghimself@gmail.com>
2018-04-04 10:01:57 -0700

    Analyses tool now exports top40 table Updated tutorial files - still need to add config file to Counts directory for detoxS

--- biodepot/orangebiodepot/OWDtoxSAnalyses.py
+++ biodepot/orangebiodepot/OWDtoxSAnalyses.py
@@ -44,11 +44,11 @@ class OWDtoxsAnalysis(OWBwBWidget):
     def handleInputsTrigger(self, value, sourceId=None):
         self.handleInputs(value, "Trigger", sourceId=None)
     def handleOutputs(self):
-        outputValue="/dat"
-        if hasattr(self,"ResultsDirectory"):
-            outputValue=getattr(self,"ResultsDirectory")
-        self.send("ResultsDirectory", outputValue)
-        outputValue=None
-        if hasattr(self,"Top40"):
-            outputValue=getattr(self,"Top40")
-        self.send("Top40", outputValue)
+        resultsDir=os.path.join(self.RepositoryDirectory,'Results') 
+        tsvFile = os.path.join(resultsDir, 'FDR-0.1/TOP-40.tsv');
+        tsvReader = FileFormat.get_reader(tsvFile)
+        data = None
+        data = tsvReader.read()
+        self.send("ResultsDirectory", resultsDir)
+        self.send("Top40", data)
+
