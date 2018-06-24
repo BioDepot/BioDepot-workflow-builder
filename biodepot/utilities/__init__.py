1f7b5bfef408d1301df2099be3f41b7394b3c848
lhhung <lhhunghimself@gmail.com>
2018-06-24 12:38:33 -0700

    Removed original Bwb without sections from menu Modified tutorials with Bwb widgets from sections

--- biodepot/utilities/__init__.py
+++
@@ -1,9 +0,0 @@
-import sysconfig
-# Category metadata.
-
-# Category icon show in the menu
-ICON = "icons/toolbox2.jpg"
-
-# Background color for category background in menu
-# and widget icon background in workflow.
-BACKGROUND = "light-purple"
