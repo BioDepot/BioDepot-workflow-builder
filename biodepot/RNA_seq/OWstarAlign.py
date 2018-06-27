c48dbe17a932291c26b02571746934cdc9fcbcf4
lhhung <lhhunghimself@gmail.com>
2018-06-27 12:26:22 -0700

    Updated Star align widget

--- biodepot/RNA_seq/OWstarAlign.py
+++ biodepot/RNA_seq/OWstarAlign.py
@@ -19,8 +19,9 @@ class OWStarAlign(OWBwBWidget):
     icon = "/biodepot/RNA_seq/icons/staralign.png"
     want_main_area = False
     docker_image_name = "biodepot/alpine-star"
-    docker_image_tag = "latest"
-    inputs = [("trigger",str,"handleInputstrigger")]
+    docker_image_tag = "3.7-2.5.2b"
+    inputs = [("trigger",str,"handleInputstrigger"),("outFileNamePrefix",str,"handleInputsoutFileNamePrefix")]
+    outputs = [("outFileNamePrefix",str),("genomeDir",str)]
     pset=functools.partial(settings.Setting,schema_only=True)
     runMode=pset(0)
     runTriggers=pset([])
@@ -148,3 +149,14 @@ class OWStarAlign(OWBwBWidget):
         self.drawGUI()
     def handleInputstrigger(self, value, sourceId=None):
         self.handleInputs(value, "trigger", sourceId=None)
+    def handleInputsoutFileNamePrefix(self, value, sourceId=None):
+        self.handleInputs(value, "outFileNamePrefix", sourceId=None)
+    def handleOutputs(self):
+        outputValue="/data"
+        if hasattr(self,"outFileNamePrefix"):
+            outputValue=getattr(self,"outFileNamePrefix")
+        self.send("outFileNamePrefix", outputValue)
+        outputValue="/data/GenomeDir"
+        if hasattr(self,"genomeDir"):
+            outputValue=getattr(self,"genomeDir")
+        self.send("genomeDir", outputValue)
