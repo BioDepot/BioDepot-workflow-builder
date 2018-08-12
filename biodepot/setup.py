e9b7d81ddadc944a074b3bb9a222d314d6aa3bdb
lhhung <lhhunghimself@gmail.com>
2018-08-12 00:32:38 -0700

    removed unused lines from biodepot/setup.py

--- biodepot/setup.py
+++ biodepot/setup.py
@@ -2,24 +2,20 @@ from setuptools import setup
 setup(name="RNA-seq",
       packages=["RNA_seq"],
       package_data={"RNA_seq": ["icons/*.svg"]},
-      classifiers=["Example :: Invalid"],
       entry_points={"orange.widgets": "RNA-seq = RNA_seq"},
       )
 setup(name="Utilities",
       packages=["Utilities"],
       package_data={"Utilities": ["icons/*.svg"]},
-      classifiers=["Example :: Invalid"],
       entry_points={"orange.widgets": "Utilities = Utilities"},
       )
 setup(name="Miscellaneous",
       packages=["Miscellaneous"],
       package_data={"Miscellaneous": ["icons/*.svg"]},
-      classifiers=["Example :: Invalid"],
       entry_points={"orange.widgets": "Miscellaneous = Miscellaneous"},
       )
 setup(name="User",
       packages=["User"],
       package_data={"User": ["icons/*.svg"]},
-      classifiers=["Example :: Invalid"],
       entry_points={"orange.widgets": "User = User"},
       )
