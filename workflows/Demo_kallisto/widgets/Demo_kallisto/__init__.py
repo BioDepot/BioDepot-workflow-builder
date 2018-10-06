a29732496dc42839106d7700cc26f11e9024bea3
lhhung <lhhunghimself@gmail.com>
2018-10-06 00:42:11 -0700

    Fixed bugs in kallisto workflows

--- workflows/Demo_kallisto/widgets/Demo_kallisto/__init__.py
+++ workflows/Demo_kallisto/widgets/Demo_kallisto/__init__.py
@@ -1,3 +1,9 @@
 import sysconfig
+# Category metadata.
+
+# Category icon show in the menu
 ICON = "icon/kallistoDemo.png"
+
+# Background color for category background in menu
+# and widget icon background in workflow.
 BACKGROUND = "light-blue"
