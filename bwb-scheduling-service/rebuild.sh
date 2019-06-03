73005778cd180cb462bbd32f7b114b63cac18b91
Hong Hung <lhhunghimself@gmail.com>
2019-06-03 08:50:58 -0700

    Launch bwb and scheduler as combined service with docker-compose

--- bwb-scheduling-service/rebuild.sh
+++ bwb-scheduling-service/rebuild.sh
@@ -4,4 +4,4 @@ MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 if [ -z "$MY_PATH" ] ; then
   exit 1  
 fi
-cd $MY_PATH && docker build -t biodepot/bwb-scheduling-service . && docker build -t local_agent -f Dockerfile-agent .
+cd $MY_PATH && docker build -t biodepot/bwb-scheduling-service . && docker build -t biodepot/local-agent -f Dockerfile-agent .
