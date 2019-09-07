f68365412269e06ae6a3c16fb6f463a27dfd85e8
Hong Hung <lhhunghimself@gmail.com>
2019-09-07 00:41:23 -0700

    fixed rename

--- bwb-scheduling-service/docker-compose-samples/local/agent-dockerfile/logDocker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-container=$1
-logDir=$2
-worker=$3
-
-pwd 2>&1 | tee -a ${logDir}/log${worker} >> ${logDir}/log0
-
-docker start -a -i $container 2>&1 | tee -a ${logDir}/log${worker} >> ${logDir}/log0
