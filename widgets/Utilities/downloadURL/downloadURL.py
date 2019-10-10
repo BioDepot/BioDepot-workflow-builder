dd7b0d3a356e54ec192044822090d59401eaba7b
Hong Hung <lhhunghimself@gmail.com>
2019-10-10 00:40:02 -0700

    updated downloadurl widget to return correct error code

--- widgets/Utilities/downloadURL/downloadURL.py
+++ widgets/Utilities/downloadURL/downloadURL.py
@@ -19,7 +19,7 @@ class OWdownloadURL(OWBwBWidget):
     icon = getIconName(__file__, "download.png")
     want_main_area = False
     docker_image_name = "biodepot/downloadurl"
-    docker_image_tag = "alpine-3.7__072818"
+    docker_image_tag = "alpine-3.7__101019"
     inputs = [
         ("directory", str, "handleInputsdirectory"),
         ("trigger", str, "handleInputstrigger"),
