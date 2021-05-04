2ce56da6bf86db958f7ad4d36e1430176307ef59
Hong Hung <lhhunghimself@gmail.com>
2021-05-03 23:37:49 -0700

    updated novnc

--- noVNC/tests/assertions.js
+++ noVNC/tests/assertions.js
@@ -1,24 +1,102 @@
-// some useful assertions for noVNC
+// noVNC specific assertions
 chai.use(function (_chai, utils) {
-    _chai.Assertion.addMethod('displayed', function (target_data) {
-        var obj = this._obj;
-        var data_cl = obj._drawCtx.getImageData(0, 0, obj._viewportLoc.w, obj._viewportLoc.h).data;
-        // NB(directxman12): PhantomJS 1.x doesn't implement Uint8ClampedArray, so work around that
-        var data = new Uint8Array(data_cl);
-        this.assert(utils.eql(data, target_data),
-            "expected #{this} to have displayed the image #{exp}, but instead it displayed #{act}",
-            "expected #{this} not to have displayed the image #{act}",
-            target_data,
-            data);
+    function _equal(a, b) {
+        return a === b;
+    }
+    _chai.Assertion.addMethod('displayed', function (targetData, cmp=_equal) {
+        const obj = this._obj;
+        const ctx = obj._target.getContext('2d');
+        const data = ctx.getImageData(0, 0, obj._target.width, obj._target.height).data;
+        const len = data.length;
+        new chai.Assertion(len).to.be.equal(targetData.length, "unexpected display size");
+        let same = true;
+        for (let i = 0; i < len; i++) {
+            if (!cmp(data[i], targetData[i])) {
+                same = false;
+                break;
+            }
+        }
+        if (!same) {
+            // eslint-disable-next-line no-console
+            console.log("expected data: %o, actual data: %o", targetData, data);
+        }
+        this.assert(same,
+                    "expected #{this} to have displayed the image #{exp}, but instead it displayed #{act}",
+                    "expected #{this} not to have displayed the image #{act}",
+                    targetData,
+                    data);
     });
 
-    _chai.Assertion.addMethod('sent', function (target_data) {
-        var obj = this._obj;
-        var data = obj._websocket._get_sent_data();
-        this.assert(utils.eql(data, target_data),
-            "expected #{this} to have sent the data #{exp}, but it actually sent #{act}",
-            "expected #{this} not to have sent the data #{act}",
-            target_data,
-            data);
+    _chai.Assertion.addMethod('sent', function (targetData) {
+        const obj = this._obj;
+        obj.inspect = () => {
+            const res = { _websocket: obj._websocket, rQi: obj._rQi, _rQ: new Uint8Array(obj._rQ.buffer, 0, obj._rQlen),
+                          _sQ: new Uint8Array(obj._sQ.buffer, 0, obj._sQlen) };
+            res.prototype = obj;
+            return res;
+        };
+        const data = obj._websocket._getSentData();
+        let same = true;
+        if (data.length != targetData.length) {
+            same = false;
+        } else {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i] != targetData[i]) {
+                    same = false;
+                    break;
+                }
+            }
+        }
+        if (!same) {
+            // eslint-disable-next-line no-console
+            console.log("expected data: %o, actual data: %o", targetData, data);
+        }
+        this.assert(same,
+                    "expected #{this} to have sent the data #{exp}, but it actually sent #{act}",
+                    "expected #{this} not to have sent the data #{act}",
+                    Array.prototype.slice.call(targetData),
+                    Array.prototype.slice.call(data));
+    });
+
+    _chai.Assertion.addProperty('array', function () {
+        utils.flag(this, 'array', true);
+    });
+
+    _chai.Assertion.overwriteMethod('equal', function (_super) {
+        return function assertArrayEqual(target) {
+            if (utils.flag(this, 'array')) {
+                const obj = this._obj;
+
+                let same = true;
+
+                if (utils.flag(this, 'deep')) {
+                    for (let i = 0; i < obj.length; i++) {
+                        if (!utils.eql(obj[i], target[i])) {
+                            same = false;
+                            break;
+                        }
+                    }
+
+                    this.assert(same,
+                                "expected #{this} to have elements deeply equal to #{exp}",
+                                "expected #{this} not to have elements deeply equal to #{exp}",
+                                Array.prototype.slice.call(target));
+                } else {
+                    for (let i = 0; i < obj.length; i++) {
+                        if (obj[i] != target[i]) {
+                            same = false;
+                            break;
+                        }
+                    }
+
+                    this.assert(same,
+                                "expected #{this} to have elements equal to #{exp}",
+                                "expected #{this} not to have elements equal to #{exp}",
+                                Array.prototype.slice.call(target));
+                }
+            } else {
+                _super.apply(this, arguments);
+            }
+        };
     });
 });
--- noVNC/utils/launch.sh
+++ noVNC/utils/launch.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Copyright (C) 2018 The noVNC Authors
+# Licensed under MPL 2.0 or any later version (see LICENSE.txt)
+
 usage() {
     if [ "$*" ]; then
         echo "$*"
         echo
     fi
-    echo "Usage: ${NAME} [--listen PORT] [--vnc VNC_HOST:PORT] [--cert CERT]"
+    echo "Usage: ${NAME} [--listen PORT] [--vnc VNC_HOST:PORT] [--cert CERT] [--ssl-only]"
     echo
     echo "Starts the WebSockets proxy and a mini-webserver and "
     echo "provides a cut-and-paste URL to go to."
@@ -14,10 +17,23 @@ usage() {
     echo "                          Default: 6080"
     echo "    --vnc VNC_HOST:PORT   VNC server host:port proxy target"
     echo "                          Default: localhost:5900"
-    echo "    --cert CERT           Path to combined cert/key file"
+    echo "    --cert CERT           Path to combined cert/key file, or just"
+    echo "                          the cert file if used with --key"
     echo "                          Default: self.pem"
+    echo "    --key KEY             Path to key file, when not combined with cert"
     echo "    --web WEB             Path to web files (e.g. vnc.html)"
     echo "                          Default: ./"
+    echo "    --ssl-only            Disable non-https connections."
+    echo "                                    "
+    echo "    --record FILE         Record traffic to FILE.session.js"
+    echo "                                    "
+    echo "    --syslog SERVER       Can be local socket such as /dev/log, or a UDP host:port pair."
+    echo "                                    "
+    echo "    --heartbeat SEC       send a ping to the client every SEC seconds"
+    echo "    --timeout SEC         after SEC seconds exit when not connected"
+    echo "    --idle-timeout SEC    server exits after SEC seconds if there are no"
+    echo "                          active connections"
+    echo "                                    "
     exit 2
 }
 
@@ -27,8 +43,15 @@ HERE="$(cd "$(dirname "$REAL_NAME")" && pwd)"
 PORT="6080"
 VNC_DEST="localhost:5900"
 CERT=""
+KEY=""
 WEB=""
 proxy_pid=""
+SSLONLY=""
+RECORD_ARG=""
+SYSLOG_ARG=""
+HEARTBEAT_ARG=""
+IDLETIMEOUT_ARG=""
+TIMEOUT_ARG=""
 
 die() {
     echo "$*"
@@ -54,7 +77,14 @@ while [ "$*" ]; do
     --listen)  PORT="${OPTARG}"; shift            ;;
     --vnc)     VNC_DEST="${OPTARG}"; shift        ;;
     --cert)    CERT="${OPTARG}"; shift            ;;
+    --key)     KEY="${OPTARG}"; shift             ;;
     --web)     WEB="${OPTARG}"; shift            ;;
+    --ssl-only) SSLONLY="--ssl-only"             ;;
+    --record) RECORD_ARG="--record ${OPTARG}"; shift ;;
+    --syslog) SYSLOG_ARG="--syslog ${OPTARG}"; shift ;;
+    --heartbeat) HEARTBEAT_ARG="--heartbeat ${OPTARG}"; shift ;;
+    --idle-timeout) IDLETIMEOUT_ARG="--idle-timeout ${OPTARG}"; shift ;;
+    --timeout) TIMEOUT_ARG="--timeout ${OPTARG}"; shift ;;
     -h|--help) usage                              ;;
     -*) usage "Unknown chrooter option: ${param}" ;;
     *) break                                      ;;
@@ -62,11 +92,14 @@ while [ "$*" ]; do
 done
 
 # Sanity checks
-which netstat >/dev/null 2>&1 \
-    || die "Must have netstat installed"
-
-netstat -ltn | grep -qs "${PORT} .*LISTEN" \
-    && die "Port ${PORT} in use. Try --listen PORT"
+if bash -c "exec 7<>/dev/tcp/localhost/${PORT}" &> /dev/null; then
+    exec 7<&-
+    exec 7>&-
+    die "Port ${PORT} in use. Try --listen PORT"
+else
+    exec 7<&-
+    exec 7>&-
+fi
 
 trap "cleanup" TERM QUIT INT EXIT
 
@@ -102,24 +135,34 @@ else
     echo "Warning: could not find self.pem"
 fi
 
+# Check key file
+if [ -n "${KEY}" ]; then
+    if [ ! -e "${KEY}" ]; then
+        die "Could not find ${KEY}"
+    fi
+fi
+
 # try to find websockify (prefer local, try global, then download local)
-if [[ -e ${HERE}/websockify ]]; then
+if [[ -d ${HERE}/websockify ]]; then
     WEBSOCKIFY=${HERE}/websockify/run
 
     if [[ ! -x $WEBSOCKIFY ]]; then
         echo "The path ${HERE}/websockify exists, but $WEBSOCKIFY either does not exist or is not executable."
-        echo "If you inteded to use an installed websockify package, please remove ${HERE}/websockify."
+        echo "If you intended to use an installed websockify package, please remove ${HERE}/websockify."
         exit 1
     fi
 
     echo "Using local websockify at $WEBSOCKIFY"
 else
-    WEBSOCKIFY=$(which websockify 2>/dev/null)
+    WEBSOCKIFY_FROMSYSTEM=$(which websockify 2>/dev/null)
+    WEBSOCKIFY_FROMSNAP=${HERE}/../usr/bin/python2-websockify
+    [ -f $WEBSOCKIFY_FROMSYSTEM ] && WEBSOCKIFY=$WEBSOCKIFY_FROMSYSTEM
+    [ -f $WEBSOCKIFY_FROMSNAP ] && WEBSOCKIFY=$WEBSOCKIFY_FROMSNAP
 
-    if [[ $? -ne 0 ]]; then
+    if [ ! -f "$WEBSOCKIFY" ]; then
         echo "No installed websockify, attempting to clone websockify..."
         WEBSOCKIFY=${HERE}/websockify/run
-        git clone https://github.com/kanaka/websockify ${HERE}/websockify
+        git clone https://github.com/novnc/websockify ${HERE}/websockify
 
         if [[ ! -e $WEBSOCKIFY ]]; then
             echo "Unable to locate ${HERE}/websockify/run after downloading"
@@ -134,7 +177,7 @@ fi
 
 echo "Starting webserver and WebSockets proxy on port ${PORT}"
 #${HERE}/websockify --web ${WEB} ${CERT:+--cert ${CERT}} ${PORT} ${VNC_DEST} &
-${WEBSOCKIFY} --web ${WEB} ${CERT:+--cert ${CERT}} ${PORT} ${VNC_DEST} &
+${WEBSOCKIFY} ${SYSLOG_ARG} ${SSLONLY} --web ${WEB} ${CERT:+--cert ${CERT}} ${KEY:+--key ${KEY}} ${PORT} ${VNC_DEST} ${HEARTBEAT_ARG} ${IDLETIMEOUT_ARG} ${RECORD_ARG} ${TIMEOUT_ARG} &
 proxy_pid="$!"
 sleep 1
 if ! ps -p ${proxy_pid} >/dev/null; then
@@ -144,7 +187,12 @@ if ! ps -p ${proxy_pid} >/dev/null; then
 fi
 
 echo -e "\n\nNavigate to this URL:\n"
-echo -e "    http://$(hostname):${PORT}/vnc.html?host=$(hostname)&port=${PORT}\n"
+if [ "x$SSLONLY" == "x" ]; then
+    echo -e "    http://$(hostname):${PORT}/vnc.html?host=$(hostname)&port=${PORT}\n"
+else
+    echo -e "    https://$(hostname):${PORT}/vnc.html?host=$(hostname)&port=${PORT}\n"
+fi
+
 echo -e "Press Ctrl-C to exit\n\n"
 
 wait ${proxy_pid}
