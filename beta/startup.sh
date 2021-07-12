9115e6e309f3e9eb89a29178a292722e361303a5
Hong Hung <lhhunghimself@gmail.com>
2021-07-12 13:07:19 -0700

    Added back the check for display variable check in startup for the beta

--- beta/startup.sh
+++ beta/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Determine an available socket
-#((display=$(ls -rv /tmp/.X11-unix/ | grep -om1 '[0-9]*') + 1))
-#sed -i "/^command/ s/ :[0-9]\+/ :${display}/" /etc/supervisor/conf.d/supervisord.conf
+((display=$(ls -rv /tmp/.X11-unix/ | grep -om1 '[0-9]*') + 1))
+sed -i "/^command/ s/ :[0-9]\+/ :${display}/" /etc/supervisor/conf.d/supervisord.conf
 
 mkdir -p /var/run/sshd
 cd /web && ./run.py > /var/log/web.log 2>&1 &
