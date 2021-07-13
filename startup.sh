4ee93abe2d79465755e82a145af3b6a6e1e79fd4
Hong Hung <lhhunghimself@gmail.com>
2021-07-12 22:07:38 -0700

    added shared flag to x11vnc for vncviewer access\nremoved buggy ncache flag

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 # Determine an available socket
 ((display=$(ls -rv /tmp/.X11-unix/ | grep -om1 '[0-9]*') + 1))
-sed -i "/^command/ s/:[0-9]\+/:${display}/" /etc/supervisor/conf.d/supervisord.conf
+sed -i "/^command/ s/ :[0-9]\+/ :${display}/" /etc/supervisor/conf.d/supervisord.conf
 
 mkdir -p /var/run/sshd
 cd /web && ./run.py > /var/log/web.log 2>&1 &
