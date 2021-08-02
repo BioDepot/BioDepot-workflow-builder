100c96ba299343e1de90484172c13c2fc19a3e7a
Hong Hung <lhhunghimself@gmail.com>
2021-08-01 20:54:22 -0700

    Fixed Jupyter ip/port options so that they are required and not optional and defaults are used

--- workflows/Demo_kallisto_jupyter/widgets/Demo_kallisto_jupyter/jupyter_sleuth/jupyter_sleuth.py
+++ workflows/Demo_kallisto_jupyter/widgets/Demo_kallisto_jupyter/jupyter_sleuth/jupyter_sleuth.py
@@ -11,83 +11,71 @@ from DockerClient import DockerClient
 from BwBase import OWBwBWidget, ConnectionDict, BwbGuiElements, getIconName, getJsonName
 from PyQt5 import QtWidgets, QtGui
 
-
 class OWjupyter_sleuth(OWBwBWidget):
     name = "jupyter_sleuth"
     description = "Base installation of Jupyter"
     priority = 103
-    icon = getIconName(__file__, "jupyter-sleuth.png")
+    icon = getIconName(__file__,"jupyter-sleuth.png")
     want_main_area = False
     docker_image_name = "biodepot/sleuth"
-    docker_image_tag = (
-        "0.30.0__ubuntu-16.04__r-3.4.4__jupyter-5.6.0__firefox-61.0.1__082318"
-    )
-    inputs = [
-        ("InputDir", str, "handleInputsInputDir"),
-        ("Trigger", str, "handleInputsTrigger"),
-        ("startingNotebook", str, "handleInputsstartingNotebook"),
-    ]
-    outputs = [("OutputDir", str), ("outputNotebook", str)]
-    pset = functools.partial(settings.Setting, schema_only=True)
-    runMode = pset(0)
-    exportGraphics = pset(False)
-    runTriggers = pset([])
-    triggerReady = pset({})
-    inputConnectionsStore = pset({})
-    optionsChecked = pset({})
-    subcommand = pset("notebook")
-    execute = pset(False)
-    startingNotebook = pset(None)
-    type = pset("notebook")
-    outputNotebook = pset(None)
-    debug = pset(False)
-    generateConfig = pset(False)
-    autoyes = pset(True)
-    allowRoot = pset(True)
-    loglevel = pset("30")
-    ip = pset("0.0.0.0")
-    port = pset(8888)
-    config = pset(None)
-    transport = pset(None)
-    keyfile = pset(None)
-    certfile = pset(None)
-    clientca = pset(None)
-    nomathjax = pset(False)
-    browser = pset(None)
-
+    docker_image_tag = "0.30.0__ubuntu-16.04__r-3.4.4__jupyter-5.6.0__firefox-61.0.1__082318"
+    inputs = [("InputDir",str,"handleInputsInputDir"),("Trigger",str,"handleInputsTrigger"),("startingNotebook",str,"handleInputsstartingNotebook")]
+    outputs = [("OutputDir",str),("outputNotebook",str)]
+    pset=functools.partial(settings.Setting,schema_only=True)
+    runMode=pset(0)
+    exportGraphics=pset(False)
+    runTriggers=pset([])
+    triggerReady=pset({})
+    inputConnectionsStore=pset({})
+    optionsChecked=pset({})
+    subcommand=pset("notebook")
+    execute=pset(False)
+    startingNotebook=pset(None)
+    type=pset("notebook")
+    outputNotebook=pset(None)
+    debug=pset(False)
+    generateConfig=pset(False)
+    autoyes=pset(True)
+    allowRoot=pset(True)
+    loglevel=pset("30")
+    ip=pset("0.0.0.0")
+    port=pset(8888)
+    config=pset(None)
+    transport=pset(None)
+    keyfile=pset(None)
+    certfile=pset(None)
+    clientca=pset(None)
+    nomathjax=pset(False)
+    browser=pset(None)
     def __init__(self):
         super().__init__(self.docker_image_name, self.docker_image_tag)
-        with open(getJsonName(__file__, "jupyter_sleuth")) as f:
-            self.data = jsonpickle.decode(f.read())
+        with open(getJsonName(__file__,"jupyter_sleuth")) as f:
+            self.data=jsonpickle.decode(f.read())
             f.close()
         self.initVolumes()
         self.inputConnections = ConnectionDict(self.inputConnectionsStore)
         self.drawGUI()
-
     def handleInputsInputDir(self, value, *args):
-        if args and len(args) > 0:
+        if args and len(args) > 0: 
             self.handleInputs("InputDir", value, args[0][0], test=args[0][3])
         else:
-            self.handleInputs("inputFile", value, None)
-
+            self.handleInputs("inputFile", value, None, False)
     def handleInputsTrigger(self, value, *args):
-        if args and len(args) > 0:
+        if args and len(args) > 0: 
             self.handleInputs("Trigger", value, args[0][0], test=args[0][3])
         else:
-            self.handleInputs("inputFile", value, None)
-
+            self.handleInputs("inputFile", value, None, False)
     def handleInputsstartingNotebook(self, value, *args):
-        if args and len(args) > 0:
+        if args and len(args) > 0: 
             self.handleInputs("startingNotebook", value, args[0][0], test=args[0][3])
         else:
-            self.handleInputs("inputFile", value, None)
-
+            self.handleInputs("inputFile", value, None, False)
     def handleOutputs(self):
-        outputValue = None
-        if hasattr(self, "OutputDir"):
-            outputValue = getattr(self, "OutputDir")
+        outputValue=None
+        if hasattr(self,"OutputDir"):
+            outputValue=getattr(self,"OutputDir")
         self.send("OutputDir", outputValue)
-        outputValue = None
-        if hasattr(self, "outputNotebook"):
-            outputValue = getattr(self, "outputNotebook")
+        outputValue=None
+        if hasattr(self,"outputNotebook"):
+            outputValue=getattr(self,"outputNotebook")
         self.send("outputNotebook", outputValue)
