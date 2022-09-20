970478cc1d97ad290094e6b3a201b77de7db251c
Hong Hung <lhhunghimself@gmail.com>
2022-09-20 00:58:23 -0700

    updated dev-files menu

---
+++ coreutils/splitRoutines.py
@@ -0,0 +1,15 @@
+def splitData(splitSettings):
+    ''' 
+        self.splitTypeOptions=['None','Auto','Count','Max file size','Min file size']
+        self.splitUnitOptionsCounts=['None','Auto','By lines','By slices','By reads','By bytes','By kB','By MB']
+        self.splitUnitOptionsValues=['None','Auto','lines','slices','reads','bytes','kB','MB']
+        self.splitCmdOptions=['None','Auto','For list','For image','For fastq','For SAM','For BAM']
+    '''
+    splitCmds=splitSettings[split]
+    return
+def splitList(var,varType):
+    #if type is directory - get a list and return
+    #if it is a list - just return list
+    if varType is "dir":
+
+    return
