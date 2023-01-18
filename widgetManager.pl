28db677a1951389e5710c451cf2dda227b343133
Hong Hung <lhhunghimself@gmail.com>
2023-01-18 13:19:38 -0800

    removed temporary work files

--- widgetManager.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-my $repo="/home/lhhung/ssh_repos/workflows";
-my @dirs=split(/\n/,`find $repo -type f -name *.ows -exec dirname {} \\;`);
-foreach my $dir (@dirs){
-    print "$dir\n";
-}
