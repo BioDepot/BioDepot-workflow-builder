584813ede7405bb0847e73d31e511e34938a0c59
lhhung <lhhunghimself@gmail.com>
2023-02-05 22:28:12 -0800

    updated install.R in sleuth Dockerfiles

--- workflows/Demo_kallisto/widgets/Demo_kallisto/sleuth/Dockerfiles/install_18.04.R
+++
@@ -1,4 +0,0 @@
-source('http://bioconductor.org/biocLite.R')
-biocLite(c("remotes","withr","rstudioapi"),suppressUpdates=TRUE)
-biocLite("devtools",suppressUpdates=TRUE)
-biocLite("pachterlab/sleuth")
