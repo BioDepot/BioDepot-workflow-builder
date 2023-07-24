a474e49676bd9cbc61890e8fc9f6c72fe0c530fa
Hong Hung <lhhunghimself@gmail.com>
2023-07-23 17:37:08 -0700

    update STAR container name

--- workflows/Demo_STAR/widgets/Demo_STAR/starIndex/starIndex.py
+++ workflows/Demo_STAR/widgets/Demo_STAR/starIndex/starIndex.py
@@ -19,7 +19,7 @@ class OWstarIndex(OWBwBWidget):
     icon = getIconName(__file__, "starIndex.png")
     want_main_area = False
     docker_image_name = "biodepot/star"
-    docker_image_tag = "2.6.0c__bookworm-slim__70da0980__5e335372__9a47e609"
+    docker_image_tag = "2.6.0c__bookworm-slim__d0d926bb__fd4cc2fa__4debfaea"
     inputs = [("Trigger", str, "handleInputsTrigger")]
     outputs = [("genomeDir", str)]
     pset = functools.partial(settings.Setting, schema_only=True)
