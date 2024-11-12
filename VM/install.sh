1776a24d7dc10bed5572617a399606f848cc8687
Hong Hung <lhhunghimself@gmail.com>
2024-11-11 22:42:44 -0800

    modified install.sh

--- VM/install.sh
+++ VM/install.sh
@@ -35,9 +35,10 @@ DOCKER_GPG=/etc/apt/keyrings/docker.gpg
         xvfb \
         zlib1g-dev \
         zenity
-  git clone https://github.com/BioDepot/BioDepot-workflow-builder.git
-  cp -r Biodepot-workflow-builder/orange3 /orange3
-  cp -r Biodepot-workflow-builder/orangePatches /orangePatches
+
+  #cd / &&  git clone https://github.com/BioDepot/BioDepot-workflow-builder.git 
+  cp -r BioDepot-workflow-builder/orange3 /orange3
+  cp -r BioDepot-workflow-builder/orangePatches /orangePatches
 apt-get install -y \
         build-essential \
         python3-dev \
@@ -46,7 +47,7 @@ apt-get install -y \
         pip==20.0.1 \
         setuptools \
         wheel 
- cd / && pip3 install -r orange3/requirements-gui.txt 
+pip3 install -r orange3/requirements-gui.txt \
          beautifulsoup4 \
          docker \
          pysam 
@@ -64,20 +65,21 @@ arch=$(dpkg --print-architecture) \
          docker-ce \
          docker-ce-cli
  pip3 install --user jsonpickle
- cp -r Biodepot-workflow-builder/widgets /widgets
- cp -r Biodepot-workflow-builder/biodepot /biodepot
- cp -r Biodepot-workflow-builder/coreutils /coreutils
+ cp -r BioDepot-workflow-builder/widgets /widgets
+ cp -r BioDepot-workflow-builder/biodepot /biodepot
+ cp -r BioDepot-workflow-builder/coreutils /coreutils
 
  apt-get install -y xorg 
- cp -r Biodepot-workflow-builder/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
+ cp -r BioDepot-workflow-builder/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
  chmod +x /usr/local/bin/generate_setup.sh
  pip3 install -e /biodepot
  curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
      && chmod +x /usr/local/bin/docker-compose
  sed -i 's/"Orange Canvas"/"Bwb"/' /orange3/Orange/canvas/config.py
  rm -rf ~/.fluxbox
- cp -r Biodepot-workflow-builder/fluxbox_config ~/.fluxbox
- cp -r Biodepot-workflow-builder/user_config/* ~/. 
+ rm -rf ~/.config
+ cp -r BioDepot-workflow-builder/fluxbox_config ~/.fluxbox
+ cp -r BioDepot-workflow-builder/user_config/* ~/. 
  cp /orangePatches/schemeedit.py /orange3/Orange/canvas/document/schemeedit.py
  cp /orangePatches/canvasmain.py /orange3/Orange/canvas/application/canvasmain.py
  cp /orangePatches/widgetsscheme.py /orange3/Orange/canvas/scheme/widgetsscheme.py
@@ -88,34 +90,34 @@ arch=$(dpkg --print-architecture) \
  cp /orangePatches/__main__.py /orange3/Orange/canvas/__main__.py
  cp /orangePatches/discovery.py /orange3/Orange/canvas/registry/discovery.py
 
-cp Biodepot-workflow-builder/scripts/startBwb.sh /usr/local/bin/startBwb.sh
-cp Biodepot-workflow-builder/scripts/startSingleBwb.sh /usr/local/bin/startSingleBwb.sh
-cp Biodepot-workflow-builder/scripts/runDockerJob.sh /usr/local/bin/runDockerJob.sh
-cp Biodepot-workflow-builder/scripts/startScheduler.sh /usr/local/bin/startScheduler.sh
-cp Biodepot-workflow-builder/scripts/build_workflow_containers.sh /usr/local/bin/build_workflow_containers.sh
-cp Biodepot-workflow-builder/scripts/whiteListToolDock.py /usr/local/bin/whiteListToolDock.py
-cp Biodepot-workflow-builder/scripts/addWorkflowsToToolDock.py /usr/local/bin/addWorkflowsToToolDock.py
-cp Biodepot-workflow-builder/scripts/addWidgetToToolDock.sh /usr/local/bin/addWidgetToToolDock.sh
-cp Biodepot-workflow-builder/scripts/removeWidgetFromToolDock.sh /usr/local/bin/removeWidgetFromToolDock.sh
-cp Biodepot-workflow-builder/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
-cp -r Biodepot-workflow-builder/executables /usr/local/bin/executables
+cp BioDepot-workflow-builder/scripts/startBwb.sh /usr/local/bin/startBwb.sh
+cp BioDepot-workflow-builder/scripts/startSingleBwb.sh /usr/local/bin/startSingleBwb.sh
+cp BioDepot-workflow-builder/scripts/runDockerJob.sh /usr/local/bin/runDockerJob.sh
+cp BioDepot-workflow-builder/scripts/startScheduler.sh /usr/local/bin/startScheduler.sh
+cp BioDepot-workflow-builder/scripts/build_workflow_containers.sh /usr/local/bin/build_workflow_containers.sh
+cp BioDepot-workflow-builder/scripts/whiteListToolDock.py /usr/local/bin/whiteListToolDock.py
+cp BioDepot-workflow-builder/scripts/addWorkflowsToToolDock.py /usr/local/bin/addWorkflowsToToolDock.py
+cp BioDepot-workflow-builder/scripts/addWidgetToToolDock.sh /usr/local/bin/addWidgetToToolDock.sh
+cp BioDepot-workflow-builder/scripts/removeWidgetFromToolDock.sh /usr/local/bin/removeWidgetFromToolDock.sh
+cp BioDepot-workflow-builder/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
+cp -r BioDepot-workflow-builder/executables /usr/local/bin/executables
 groupadd ftpaccess
-cp Biodepot-workflow-builder/sshd_config /etc/ssh/sshd_config
-cp Biodepot-workflow-builder/startSftp.sh /usr/local/bin/startSftp.sh
-cp Biodepot-workflow-builder/scripts/findResolution.sh /usr/local/bin/findResolution.sh
+cp BioDepot-workflow-builder/sshd_config /etc/ssh/sshd_config
+cp BioDepot-workflow-builder/startSftp.sh /usr/local/bin/startSftp.sh
+cp BioDepot-workflow-builder/scripts/findResolution.sh /usr/local/bin/findResolution.sh
 
-cp -r Biodepot-workflow-builder/workflows /workflows
-cp -r Biodepot-workflow-builder/notebooks /notebooks
-cp -r Biodepot-workflow-builder/templates /templates
-cp -r Biodepot-workflow-builder/icons /icons
-cp -r Biodepot-workflow-builder/tutorialFiles /tutorialFiles
-cp Biodepot-workflow-builder/serverSettings.json /biodepot
+cp -r BioDepot-workflow-builder/workflows /workflows
+cp -r BioDepot-workflow-builder/notebooks /notebooks
+cp -r BioDepot-workflow-builder/templates /templates
+cp -r BioDepot-workflow-builder/icons /icons
+cp -r BioDepot-workflow-builder/tutorialFiles /tutorialFiles
+cp BioDepot-workflow-builder/serverSettings.json /biodepot
 
-cp Biodepot-workflow-builder/startup.sh /
+cp BioDepot-workflow-builder/startup.sh /
 
-#cp Biodepot-workflow-builder/ config files for dev tools
-cp -r Biodepot-workflow-builder/dev-files/geany/ /root/.config/
-cp -r Biodepot-workflow-builder/VM/xorg.conf /etc/X11/xorg.conf
-cp -r Biodepot-workflow-builder/VM/*.sh /usr/local/bin/
-cp Biodepot-workflow-builder/VM/menu /root/.fluxbox/menu
-cp /root/.fluxbox/bwb.svg /orange3/Orange/canvas/icons/.
+#cp BioDepot-workflow-builder/ config files for dev tools
+cp -r BioDepot-workflow-builder/dev-files/geany/ /root/.config/
+cp -r BioDepot-workflow-builder/VM/xorg.conf /etc/X11/xorg.conf
+cp -r BioDepot-workflow-builder/VM/*.sh /usr/local/bin/
+cp BioDepot-workflow-builder/VM/menu /root/.fluxbox/menu
+cp /root/.fluxbox/bwb.svg /orange3/Orange/canvas/icons/orange-canvas.svg
