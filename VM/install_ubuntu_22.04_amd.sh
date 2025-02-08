60a1b20b7ee4d13281b2dc872351d5f3039a3bf9
Hong Hung <lhhunghimself@gmail.com>
2025-02-08 12:59:30 -0800

    removed sudo from install script for AMD

--- VM/install_ubuntu_22.04_amd.sh
+++ VM/install_ubuntu_22.04_amd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #cd /home/$USERNAME
 #git clone https://github.com/BioDepot/BioDepot-workflow-builder.git
-#Then run this script using sudo
-#sudo /home/$USERNAME/BioDepot-workflow-builder/VM/install_ubuntu_22.04.sh
+#Then run this script using 
+# /home/$USERNAME/BioDepot-workflow-builder/VM/install_ubuntu_22.04.sh
 set -e
 USERNAME=ubuntu
 DOCKER_GPG=/etc/apt/keyrings/docker.gpg
@@ -10,9 +10,9 @@ DOCKER_GPG=/etc/apt/keyrings/docker.gpg
 
 BIODEPOT=/home/$USERNAME/BioDepot-workflow-builder
 export DEBIAN_FRONTEND=noninteractive
-echo "package_name package_name/option select value" | sudo debconf-set-selections
-sudo apt-get -y update
-sudo apt-get install -y --no-install-recommends \
+echo "package_name package_name/option select value" |  debconf-set-selections
+ apt-get -y update
+ apt-get install -y --no-install-recommends \
         curl \
         build-essential \
         dbus-x11 \
@@ -55,72 +55,72 @@ python3.12 -m venv /venv
 source /venv/bin/activate
 python3.12 -m ensurepip --upgrade
 python3.12 -m pip install --upgrade pip setuptools 
-sudo  rsync -av  $BIODEPOT/VM/orange3/ /orange3/
-sudo pip3 install -r /orange3/requirements-gui.txt \
+  rsync -av  $BIODEPOT/VM/orange3/ /orange3/
+ pip3 install -r /orange3/requirements-gui.txt \
          beautifulsoup4 \
          docker \
          pysam
-sudo pip3 install -e /orange3
+ pip3 install -e /orange3
 
-sudo apt-get install -y --no-install-recommends gnupg 
-sudo mkdir -p /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o $DOCKER_GPG 
+ apt-get install -y --no-install-recommends gnupg 
+ mkdir -p /etc/apt/keyrings &&  curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o $DOCKER_GPG 
 arch=$(dpkg --print-architecture) \
      && codename=$(awk -F= '/CODENAME/ {print $2;exit}' /etc/os-release) \
-     && sudo echo "deb [arch=$arch signed-by=$DOCKER_GPG] https://download.docker.com/linux/ubuntu $codename stable" > \
+     &&  echo "deb [arch=$arch signed-by=$DOCKER_GPG] https://download.docker.com/linux/ubuntu $codename stable" > \
          /etc/apt/sources.list.d/docker.list \
-     && sudo apt-get update \
-     && sudo apt-get install -y --no-install-recommends \
+     &&  apt-get update \
+     &&  apt-get install -y --no-install-recommends \
          containerd.io \
          docker-ce \
          docker-ce-cli
- sudo rsync -av $BIODEPOT/widgets/ /widgets/ \
-  && sudo rsync -av $BIODEPOT/biodepot/ /biodepot/ \
-  && sudo rsync -av $BIODEPOT/VM/coreutils/ /coreutils/
+  rsync -av $BIODEPOT/widgets/ /widgets/ \
+  &&  rsync -av $BIODEPOT/biodepot/ /biodepot/ \
+  &&  rsync -av $BIODEPOT/VM/coreutils/ /coreutils/
 
- sudo apt-get install -y xorg 
- sudo cp  $BIODEPOT/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
- sudo chmod +x /usr/local/bin/generate_setup.sh
- sudo pip3 install -e /biodepot
- sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+  apt-get install -y xorg 
+  cp  $BIODEPOT/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
+  chmod +x /usr/local/bin/generate_setup.sh
+  pip3 install -e /biodepot
+  curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
      && chmod +x /usr/local/bin/docker-compose
- sudo sed -i 's/"Orange Canvas"/"Bwb"/' /orange3/Orange/canvas/config.py
- sudo rm -rf ~/.fluxbox
- sudo rm -rf ~/.config/biolab.si
- sudo rsync -av $BIODEPOT/VM/user_config/ ~/ 
+  sed -i 's/"Orange Canvas"/"Bwb"/' /orange3/Orange/canvas/config.py
+  rm -rf ~/.fluxbox
+  rm -rf ~/.config/biolab.si
+  rsync -av $BIODEPOT/VM/user_config/ ~/ 
 
-sudo cp $BIODEPOT/scripts/startSingleBwb.sh /usr/local/bin/startSingleBwb.sh
-sudo cp $BIODEPOT/scripts/runDockerJob.sh /usr/local/bin/runDockerJob.sh
-sudo cp $BIODEPOT/scripts/startScheduler.sh /usr/local/bin/startScheduler.sh
-sudo cp $BIODEPOT/scripts/build_workflow_containers.sh /usr/local/bin/build_workflow_containers.sh
-sudo cp $BIODEPOT/scripts/whiteListToolDock.py /usr/local/bin/whiteListToolDock.py
-sudo cp $BIODEPOT/scripts/addWorkflowsToToolDock.py /usr/local/bin/addWorkflowsToToolDock.py
-sudo cp $BIODEPOT/scripts/addWidgetToToolDock.sh /usr/local/bin/addWidgetToToolDock.sh
-sudo cp $BIODEPOT/scripts/removeWidgetFromToolDock.sh /usr/local/bin/removeWidgetFromToolDock.sh
-sudo cp $BIODEPOT/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
-sudo cp -r $BIODEPOT/executables /usr/local/bin/executables
-sudo groupadd ftpaccess
-sudo cp $BIODEPOT/sshd_config /etc/ssh/sshd_config
-sudo cp $BIODEPOT/startSftp.sh /usr/local/bin/startSftp.sh
-sudo cp $BIODEPOT/scripts/findResolution.sh /usr/local/bin/findResolution.sh
+ cp $BIODEPOT/scripts/startSingleBwb.sh /usr/local/bin/startSingleBwb.sh
+ cp $BIODEPOT/scripts/runDockerJob.sh /usr/local/bin/runDockerJob.sh
+ cp $BIODEPOT/scripts/startScheduler.sh /usr/local/bin/startScheduler.sh
+ cp $BIODEPOT/scripts/build_workflow_containers.sh /usr/local/bin/build_workflow_containers.sh
+ cp $BIODEPOT/scripts/whiteListToolDock.py /usr/local/bin/whiteListToolDock.py
+ cp $BIODEPOT/scripts/addWorkflowsToToolDock.py /usr/local/bin/addWorkflowsToToolDock.py
+ cp $BIODEPOT/scripts/addWidgetToToolDock.sh /usr/local/bin/addWidgetToToolDock.sh
+ cp $BIODEPOT/scripts/removeWidgetFromToolDock.sh /usr/local/bin/removeWidgetFromToolDock.sh
+ cp $BIODEPOT/scripts/generate_setup.sh /usr/local/bin/generate_setup.sh
+ cp -r $BIODEPOT/executables /usr/local/bin/executables
+ groupadd ftpaccess
+ cp $BIODEPOT/sshd_config /etc/ssh/sshd_config
+ cp $BIODEPOT/startSftp.sh /usr/local/bin/startSftp.sh
+ cp $BIODEPOT/scripts/findResolution.sh /usr/local/bin/findResolution.sh
 
-sudo cp -r $BIODEPOT/workflows /workflows
-sudo cp -r $BIODEPOT/notebooks /notebooks
-sudo cp -r $BIODEPOT/templates /templates
-sudo cp -r $BIODEPOT/icons /icons
-sudo cp -r $BIODEPOT/tutorialFiles /tutorialFiles
-sudo cp $BIODEPOT/serverSettings.json /biodepot
+ cp -r $BIODEPOT/workflows /workflows
+ cp -r $BIODEPOT/notebooks /notebooks
+ cp -r $BIODEPOT/templates /templates
+ cp -r $BIODEPOT/icons /icons
+ cp -r $BIODEPOT/tutorialFiles /tutorialFiles
+ cp $BIODEPOT/serverSettings.json /biodepot
 
-sudo cp $BIODEPOT/startup.sh /
+ cp $BIODEPOT/startup.sh /
 
-#sudo cp $BIODEPOT/ config files for dev tools
-sudo cp -r $BIODEPOT/dev-files/geany/ /root/.config/
-sudo cp -r $BIODEPOT/VM/xorg.conf /etc/X11/xorg.conf
-sudo cp -r $BIODEPOT/VM/*.sh /usr/local/bin/
-sudo cp $BIODEPOT/VM/menu /root/.fluxbox/menu
-sudo cp /root/.fluxbox/bwb.svg /orange3/Orange/canvas/icons/orange-canvas.svg
-#sudo cp -r ~/biolab.si ~/.config/
+# cp $BIODEPOT/ config files for dev tools
+ cp -r $BIODEPOT/dev-files/geany/ /root/.config/
+ cp -r $BIODEPOT/VM/xorg.conf /etc/X11/xorg.conf
+ cp -r $BIODEPOT/VM/*.sh /usr/local/bin/
+ cp $BIODEPOT/VM/menu /root/.fluxbox/menu
+ cp /root/.fluxbox/bwb.svg /orange3/Orange/canvas/icons/orange-canvas.svg
+# cp -r ~/biolab.si ~/.config/
 #for the user
-#sudo groupadd $USERNAME
-sudo usermod -aG docker $USERNAME
-sudo chown -R $USERNAME:$USERNAME /biodepot $BIODEPOT /orange3 /widgets /workflows /coreutils  /icons 
-sudo rsync -av $BIODEPOT/VM/user_config/ /home/$USERNAME/ && chown -R $USERNAME:$USERNAME /home/$USERNAME
+# groupadd $USERNAME
+ usermod -aG docker $USERNAME
+ chown -R $USERNAME:$USERNAME /biodepot $BIODEPOT /orange3 /widgets /workflows /coreutils  /icons 
+ rsync -av $BIODEPOT/VM/user_config/ /home/$USERNAME/ && chown -R $USERNAME:$USERNAME /home/$USERNAME
